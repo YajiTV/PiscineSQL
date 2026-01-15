c24812e445e1fb4e409bfc228175bc148f0b69ea
pmathys <you@example.com>
2026-01-15 19:04:42 +0100

    ok

---
+++ update-employee.sql
@@ -0,0 +1,2 @@
+UPDATE employees SET Title = 'IT Manager', ReportsTo = (SELECT EmployeeId FROM employees WHERE FirstName = 'Andrew' AND LastName = 'Adams')
+WHERE FirstName = 'Robert' AND LastName = 'King';
