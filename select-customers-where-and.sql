9601b9a91b758d69b3d6668b9d7f9cdf47893d5a
pmathys <mathys.paillekrobb@ynov.com>
2026-01-19 14:42:53 +0100

    ok

--- select-customers-where-and.sql
+++ select-customers-where-and.sql
@@ -1,3 +1,5 @@
-SELECT FirstName, LastName
-FROM customers
-WHERE Country ='USA' AND SupportRep ='Peacock Jane';
+SELECT c.FirstName, c.LastName
+FROM customers AS c
+JOIN employees AS e ON c.SupportRepId = e.EmployeeId
+
+WHERE c.Country ='USA' AND e.LastName ='Peacock' AND e.FirstName ='Jane';
