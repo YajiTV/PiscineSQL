ba447895a398f6ec50338c7dfc561e90da54688c
pmathys <mathys.paillekrobb@ynov.com>
2026-01-20 16:54:45 +0100

    ok

--- customers-invoices-avg.sql
+++ customers-invoices-avg.sql
@@ -2,8 +2,8 @@ SELECT
   c.FirstName,
   c.LastName,
   AVG(i.Total) AS InvoicesAverage
-FROM Customer AS c
-LEFT JOIN Invoice AS i
+FROM customers AS c
+LEFT JOIN invoices AS i
   ON i.CustomerId = c.CustomerId
 GROUP BY
   c.CustomerId,
