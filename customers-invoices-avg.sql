32b9fc1f123f768912431d7481642414e9d276e9
pmathys <mathys.paillekrobb@ynov.com>
2026-01-20 16:54:31 +0100

    ok

--- customers-invoices-avg.sql
+++ customers-invoices-avg.sql
@@ -6,7 +6,7 @@ FROM Customer AS c
 LEFT JOIN Invoice AS i
   ON i.CustomerId = c.CustomerId
 GROUP BY
-  c.CustomerId,  -- group by ID to avoid mixing same names
+  c.CustomerId,
   c.FirstName,
   c.LastName
 ORDER BY
