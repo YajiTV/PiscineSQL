b50f289b5d7599fe48d8c44c808502398212760b
pmathys <mathys.paillekrobb@ynov.com>
2026-01-20 17:07:02 +0100

    ok

--- select-invoice-items.sql
+++ select-invoice-items.sql
@@ -2,7 +2,7 @@ SELECT
   il.InvoiceId,
   t.Name AS InvoiceItem,
   il.UnitPrice
-FROM invoices_items AS il
+FROM invoice_items AS il
 JOIN tracks AS t
   ON t.TrackId = il.TrackId
 WHERE il.InvoiceId = 10
