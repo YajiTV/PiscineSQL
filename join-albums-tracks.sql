7e2fb010c294b9683317201c5d018fe8c1b6c752
pmathys <mathys.paillekrobb@ynov.com>
2026-01-20 15:39:05 +0100

    ok

--- join-albums-tracks.sql
+++ join-albums-tracks.sql
@@ -2,7 +2,7 @@ SELECT
   a.Title        AS AlbumName,
   t.Name         AS TrackName,
   t.Milliseconds AS Milliseconds
-FROM Album AS a
-JOIN Track AS t ON t.AlbumId = a.AlbumId
+FROM albums AS a
+JOIN tracks AS t ON t.AlbumId = a.AlbumId
 ORDER BY t.Milliseconds ASC
 LIMIT 50;
