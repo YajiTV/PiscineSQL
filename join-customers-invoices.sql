275c3f635b02382b74027c0043a3cf395aab4c3b
pmathys <mathys.paillekrobb@ynov.com>
2026-01-20 14:16:21 +0100

    ok

--- join-customers-invoices.sql
+++ join-customers-invoices.sql
@@ -3,7 +3,7 @@ SELECT
   c.LastName,
   c.Company,
   i.Total AS InvoiceTotalPrice
-FROM Customer AS c
-JOIN Invoice  AS i
+FROM customers AS c
+JOIN invoices AS i
   ON i.CustomerId = c.CustomerId
 WHERE c.Company IS NOT NULL;
