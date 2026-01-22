68b50de69a95ab5ee21976822f4548334ae90d32
pmathys <mathys.paillekrobb@ynov.com>
2026-01-22 14:22:25 +0100

    ok

--- genre-tracks-average.sql
+++ genre-tracks-average.sql
@@ -1,4 +1,4 @@
-SELECT Name, AVG(Milliseconds) AS AverageDuration
+SELECT genres.Name AS Name, AVG(tracks.Milliseconds) AS AverageDuration
 FROM tracks
 JOIN genres ON tracks.GenreId = genres.GenreId
 GROUP BY genres.Name
