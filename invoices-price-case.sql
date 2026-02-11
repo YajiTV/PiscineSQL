760ecfe2c7fe26996a7e36b8e9f5e2218e78c366
pmathys <mathys.paillekrobb@ynov.com>
2026-02-11 22:37:32 +0100

    ca marche

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -5,6 +5,6 @@ SELECT
         WHEN Total < 5 THEN 'Price lower than 5$'
         WHEN Total < 10 THEN 'Price lower than 10$'
         ELSE 'Price greater than 10$'
-    END AS PriceCategory
+    END AS "CASE"
 FROM invoices
 LIMIT 100;
