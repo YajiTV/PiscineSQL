f1d4a8a3b610937fe476ffdff90a3b8fdbb4590c
pmathys <mathys.paillekrobb@ynov.com>
2026-02-12 15:45:08 +0100

    ok

---
+++ artist-albums-case.sql
@@ -0,0 +1,11 @@
+SELECT artists.Name, 
+       COUNT(albums.AlbumId) AS NbAlbums,
+       CASE 
+           WHEN COUNT(albums.AlbumId) = 1 THEN 'Unproductive'
+           WHEN COUNT(albums.AlbumId) > 1 AND COUNT(albums.AlbumId) < 10 THEN 'Productive'
+           WHEN COUNT(albums.AlbumId) >= 10 THEN 'Very Productive'
+       END AS IsProductive
+FROM artists
+LEFT JOIN albums ON artists.ArtistId = albums.ArtistId
+GROUP BY artists.ArtistId, artists.Name
+LIMIT 100;
