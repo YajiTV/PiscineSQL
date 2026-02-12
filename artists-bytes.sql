b15f2c63f2ad985b9d63267816c7ccaafa0db288
pmathys <mathys.paillekrobb@ynov.com>
2026-02-12 15:35:22 +0100

    ok

--- artists-bytes.sql
+++ artists-bytes.sql
@@ -1,4 +1,5 @@
 SELECT artists.Name AS ArtistName, albums.Title AS AlbumName, tracks.Name AS TrackName, ROUND(tracks.Bytes / 1000000, 2) AS MegaBytes
 FROM tracks
+WHERE albums.Name = 'American Idiot'
 JOIN albums ON tracks.AlbumId = albums.AlbumId
 JOIN artists ON albums.ArtistId = artists.ArtistId
